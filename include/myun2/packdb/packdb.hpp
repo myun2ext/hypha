64b18908d35c98132818d6f914519139ba3e73e1
myun2 <myun2@nwhite.info>
2013-12-05 14:04:36 +0900

    moved

--- include/myun2/packdb/packdb.hpp
+++ include/myun2/packdb/packdb.hpp
@@ -1,4 +1,14 @@
 #ifndef __github_com_myun2__packdb__packdb_HPP__
 #define __github_com_myun2__packdb__packdb_HPP__
 
+namespace myun2
+{
+	namespace packdb
+	{
+		class pack
+		{
+		};
+	}
+}
+
 #endif//__github_com_myun2__packdb__packdb_HPP__
