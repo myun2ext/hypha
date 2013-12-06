ae5aea76da28d2902e91436a89a32116fb291433
myun2 <myun2@nwhite.info>
2013-12-06 17:18:49 +0900

    impl integer / string

--- include/myun2/packdb/types/integer.hpp
+++ include/myun2/packdb/types/integer.hpp
@@ -7,9 +7,21 @@ namespace myun2
 	{
 		namespace types
 		{
+			template <typename T>
 			class integer
 			{
+			private:
+				T value;
+			public:
+				integer() : value(0) {}
+				integer(const T& v_in) : value(v_in) {}
+
+				const void* serialize_value() const { return &value; }
+				unsigned int size() const { return sizeof(T); }
 			};
+
+			typedef integer<float> float_value;
+			typedef integer<double> double_value;
 		}
 	}
 }
--- include/myun2/packdb/types/string.hpp
+++ include/myun2/packdb/types/string.hpp
@@ -1,6 +1,8 @@
 #ifndef __github_com_myun2__packdb__type__string_HPP__
 #define __github_com_myun2__packdb__type__string_HPP__
 
+#include <string>
+
 namespace myun2
 {
 	namespace packdb
@@ -9,6 +11,16 @@ namespace myun2
 		{
 			class string
 			{
+			private:
+				typedef ::std::string impl_type;
+				impl_type value;
+			public:
+				string(){}
+				string(const ::std::string& s) : value(s) {}
+				string(const char* s) : value(s) {}
+
+				const void* serialize_value() const { return value.c_str(); }
+				unsigned int size() const { return value.size(); }
 			};
 		}
 	}
