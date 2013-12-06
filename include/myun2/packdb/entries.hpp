876a4fff478decdbfc0ab374fdbeac7fd6ef9cd8
myun2 <myun2@nwhite.info>
2013-12-06 17:31:17 +0900

    vector of eid list.

--- include/myun2/packdb/entries.hpp
+++ include/myun2/packdb/entries.hpp
@@ -1,16 +1,21 @@
 #ifndef __github_com_myun2__packdb__entries_HPP__
 #define __github_com_myun2__packdb__entries_HPP__
 
+#include <vector>
+
 namespace myun2
 {
 	namespace packdb
 	{
-		template <typename T>
+		template <typename T, typename EntryIdType=unsigned int>
 		class entries
 		{
 		private:
 			typedef T Impl;
+			typedef EntryIdType eid_type;
 			T& impl;
+
+			::std::vector<eid_type> eids;
 		public:
 			entries(Impl& i) : impl(i) {}
 		};
