845ccdf7f4f03ae1358a0e13b65d1f572f469f73
myun2 <myun2@nwhite.info>
2013-12-06 17:14:53 +0900

    packdb types of integer / string Add

---
+++ include/myun2/packdb/types/integer.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__packdb__type__integer_HPP__
+#define __github_com_myun2__packdb__type__integer_HPP__
+
+namespace myun2
+{
+	namespace packdb
+	{
+		namespace types
+		{
+			class integer
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__packdb__type__integer_HPP__
---
+++ include/myun2/packdb/types/string.hpp
@@ -0,0 +1,17 @@
+#ifndef __github_com_myun2__packdb__type__string_HPP__
+#define __github_com_myun2__packdb__type__string_HPP__
+
+namespace myun2
+{
+	namespace packdb
+	{
+		namespace types
+		{
+			class string
+			{
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__packdb__type__string_HPP__
