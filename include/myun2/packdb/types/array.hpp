cb0333daaa86117c0fb9740979088498b9e4967a
myun2 <myun2@nwhite.info>
2013-12-10 14:06:00 +0900

    implementing array

--- include/myun2/packdb/types/array.hpp
+++ include/myun2/packdb/types/array.hpp
@@ -9,16 +9,26 @@ namespace myun2
 	{
 		namespace types
 		{
-			template <typename T>
+			template <typename T, unsigned int _BlockSize=128>
 			class array
 			{
 			private:
 				::std::vector<T> value;
+				static const int item_size = sizeof(T);
 			public:
 				array(){}
 
-				const void* serialize_value() const { return &value.begin(); }
-				unsigned int size() const { return value.size() * sizeof(T); }
+				const void* serialize_value() const { return value.begin(); }
+				unsigned int size() const { return value.size(); }
+				unsigned int data_size() const { return size() * item_size; }
+
+				template <typename _Writer>
+				void serialize(_Writer& writer)
+				{
+				}
+
+				void read_serialized(const void* s, unsigned int size) {
+				}
 			};
 		}
 	}
