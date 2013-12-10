492152c78399a0fc371cbc4aa74ad5773553c164
myun2 <myun2@nwhite.info>
2013-12-10 16:03:06 +0900

    array2: indata structure

--- include/myun2/packdb/types/array2.hpp
+++ include/myun2/packdb/types/array2.hpp
@@ -7,38 +7,50 @@ namespace myun2
 	{
 		namespace types
 		{
-			template <typename T, unsigned int _BlockSize=32>
+			template <typename T, unsigned int _Size=31>
 			class array2
 			{
 			private:
-				T* p_allocated;
-				unsigned int used;
+				typedef struct indata_type {
+					unsigned char code;
+					unsigned char subcode;
+					unsigned char _reserv1;
+					unsigned char used;
+					T data[_Size];
 
-				void allocate() { p_allocated = new T [_BlockSize]; }
-				void deallocate() { delete[] p_allocated; }
+					indata_type() {
+						code = 0;
+						subcode = 0;
+						_reserv1 = 0;
+						used = 0;
+					}
+				} indata_t;
+				indata_t indata;
 
-				void used_countup() { used++; }
+				void used_countup() { indata.used++; }
 			public:
-				array2() : p_allocated(0), used(0) { allocate(); }
-				virtual ~array2() { deallocate(); }
+				array2(){}
+				virtual ~array2(){}
 
-				const T* data() const { return p_allocated; }
+				indata_type* internal() { return &indata; }
+				const indata_type* internal() const { return &indata; }
+
+				const T* data() const { return indata.data; }
 				const void* serialize_value() const { return data(); }
-				unsigned int size() const { return used; }
+				unsigned int size() const { return indata.used; }
 				unsigned int data_size() const { return size() * sizeof(T); }
 
-				T& at(unsigned int i) { return p_allocated[i + 1]; }
-				const T& at(unsigned int i) const { return p_allocated[i + 1]; }
-				T& tail() { return at(used); }
-				const T& tail() const { return at(used); }
+				T& at(unsigned int i) { return indata.data[i]; }
+				const T& at(unsigned int i) const { return indata.data[i]; }
+				T& tail() { return at(size()); }
+				const T& tail() const { return at(size()); }
 
 				const void* serialized() const {
-					p_allocated[0] = size();
-					return data();
+					return &indata;
 				}
 
 				void read_serialized(const void* s, unsigned int size) {
-					memcpy(p_allocated, s, size);
+					memcpy(&indata, s, size);
 				}
 
 				T& operator [] (unsigned int i) {
@@ -47,7 +59,7 @@ namespace myun2
 					return at(i); }
 
 				bool append(const T& v) {
-					if ( used+1 == _BlockSize )
+					if ( size() == _Size )
 						return false;
 					used_countup();
 					tail() = v;
