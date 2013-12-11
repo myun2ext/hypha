96adc87766c1403c99d82c6e0d64f3c6bb946f2e
myun2 <myun2@nwhite.info>
2013-12-11 13:58:42 +0900

    mycelium/colony.hpp Add

---
+++ include/myun2/mycelium/colony.hpp
@@ -0,0 +1,22 @@
+#ifndef __github_com_myun2__mycelium__colony_HPP__
+#define __github_com_myun2__mycelium__colony_HPP__
+
+#include <memory.h>
+
+namespace myun2
+{
+	namespace mycelium
+	{
+		template <typename _Locator>
+		class colony
+		{
+		private:
+			
+		public:
+			colony(){}
+			virtual ~colony(){}
+		};
+	}
+}
+
+#endif//__github_com_myun2__mycelium__colony_HPP__
